5c3255fb0ce138835cb0cfdfb491f9cc1e431265
Manoj Kumar <manojkumarm6m@gmail.com>
2025-11-29 13:07:47 +0530

    new recursion

---
+++ src/main/java/recursion/PrintNumbers.java
@@ -0,0 +1,14 @@
+public class PrintNumbers{
+
+    private static void printNos(int n){
+        if(n == 0) return;
+        printNos(n-1);
+        System.out.println(n);
+    }
+
+
+    public static void main(String[] args) {
+        int n = 10;
+        printNos(n);
+    }
+}
