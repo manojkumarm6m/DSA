50f0019ca80fcd130204691eba1b446b9cfa5a54
Manoj Kumar <manojkumarm6m@gmail.com>
2025-12-01 08:37:03 +0530

    new recursion

---
+++ src/main/java/recursion/RecursiveBubbleSort.java
@@ -0,0 +1,27 @@
+public class RecursiveBubbleSort {
+
+
+    public static void bubbleSort(int[] arr, int n) {
+        if(n == 1){
+            return ;
+        } 
+
+        for(int i=0; i<n-1; i++){
+            if(arr[i] > arr[i+1]){
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        bubbleSort(arr, n-1);
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+        bubbleSort(arr, arr.length);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
